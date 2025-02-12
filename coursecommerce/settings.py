be234a7945396363eafdd922d6be27a4ca7905ef
Ermand Haruni <88191097+ErmandH@users.noreply.github.com>
2025-02-12 18:21:35 +0300

    Revert "vercel_deploy_commit"
    This reverts commit 023ee8b3cbc544b58dd2e5c73043790f770f7cc6.

--- coursecommerce/settings.py
+++ coursecommerce/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-12%uiz^d_cbhcu_@!o=el*#)^#ejho6lsr1494kmb4t38)%8fd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -182,10 +181,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'courses' / 'static',
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
-STATIC_URL = "/staticfiles/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
